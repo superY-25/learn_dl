a089eabfffdff42da1ccd0cb7c60348b1e9b6cfc
supery <alonyoung@126.com>
2021-03-01 10:29:47 +0800

    新增bp网络、全连接网络以及mnist案例

---
+++ model/activators.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+"""
+激活函数py文件
+前向函数返回 激活函数值
+反向函数返回 激活函数导数值
+"""
+
+
+class ReluActivator(object):
+    def forward(self, weighted_input):
+        return max(0, weighted_input)
+
+    def backward(self, output):
+        return 1 if output > 0 else 0
+
+
+class IdentityActivator(object):
+    def forward(self, weighted_input):
+        return weighted_input
+
+    def backward(self, output):
+        return 1
+
+
+class SigmoidActivator(object):
+    def forward(self, weighted_input):
+        return 1.0 / (1.0 + np.exp(-weighted_input))
+
+    def backward(self, output):
+        return output * (1 - output)
+
+
+class TanhActivator(object):
+    def forward(self, weighted_input):
+        return 2.0 / (1.0 + np.exp(-2 * weighted_input)) - 1.0
+
+    def backward(self, output):
+        return 1 - output**2
